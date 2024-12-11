4e5114c064c52d5c27b2fe09652c041110ad193f
HOANGDINHTUNG <mtung3365@gmail.com>
2024-12-11 07:44:37 +0700

    Add files via upload

---
+++ ss2bt6.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	float a, b, triangle_edge;
+	printf("moi nhap vao do dai : ");
+	scanf("%f",&a);
+	printf("moi nhap vao chieu cao : ");
+	scanf("%f",&b);
+	triangle_edge= 0.5 * a * b;
+	printf("dien tich cua tam giac la : %.2f",triangle_edge);
+
+	
+	
+}
+
